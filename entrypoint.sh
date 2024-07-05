1412efab793560b454d90070751c4d00d0c74f6a
Mephi00 <maximhammer@icloud.com>
2024-07-05 15:31:57 +0200

    add server game settings modifier

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,20 @@ onExit() {
     wait $!
 }
 
+backwards_compatability() {
+    VR_GAME_MODE="${V_RISING_GAME_MODE:-VR_GAME_MODE}"
+    VR_DEATH_CONTAINER_PERMISSIONS="${V_RISING_DEATH_CONTAINER_PERMISSIONS:-VR_DEATH_CONTAINER_PERMISSIONS}"
+    VR_CLAN_SIZE="${V_RISING_CLAN_SIZE:-VR_CLAN_SIZE}"
+    VR_DAY_DURATION_SECONDS="${V_RISING_DAY_DURATION_SECONDS:-VR_DAY_DURATION_SECONDS}"
+    VR_DAY_START_HOUR="${V_RISING_DAY_START_HOUR:-VR_DAY_START_HOUR}"
+    VR_DAY_END_HOUR="${V_RISING_DAY_END_HOUR:-VR_DAY_END_HOUR}"
+    VR_MAX_HEALTH_MOD="${V_RISING_MAX_HEALTH_MOD:-VR_MAX_HEALTH_MOD}"
+    VR_MAX_HEALTH_GLOBAL_MOD="${V_RISING_MAX_HEALTH_GLOBAL_MOD:-VR_MAX_HEALTH_GLOBAL_MOD}"
+    VR_RESOURCE_YIELD_MOD="${V_RISING_RESOURCE_YIELD_MOD:-VR_RESOURCE_YIELD_MOD}"
+    VR_TOMB_LIMIT="${V_RISING_TOMB_LIMIT:-VR_TOMB_LIMIT}"
+    VR_NEST_LIMIT="${V_RISING_NEST_LIMIT:-VR_NEST_LIMIT}"
+}
+
 check_req_vars() {
     if [ -z "${VR_NAME}" ]; then
         echo "VR_NAME has to be set"
@@ -37,7 +51,14 @@ setServerHostSettings() {
 
     echo "Using env vars for ServerHostSettings"
 
-    VR_RCON_PASSWORD="${VR_RCON_PASSWORD:-changeme123}" \
+    VR_RCON_PASSWORD="${VR_RCON_PASSWORD:-''}" \
+    VR_ENABLE_RCON="${VR_ENABLE_RCON:-false}" \
+    VR_LIST_ON_EOS="${VR_LIST_ON_EOS:-false}" \
+    VR_LIST_ON_STEAM="${VR_LIST_ON_STEAM:-false}" \
+    VR_GAME_PORT="${VR_GAME_PORT:-9876}" \
+    VR_QUERY_PORT="${VR_QUERY_PORT:-9877}" \
+    VR_MAX_USERS="${VR_MAX_USERS:-40}" \
+    VR_MAX_ADMINS="${VR_MAX_ADMINS:-4}" \
     envsubst < /templates/ServerHostSetting.templ > $WRITE_DIR/ServerHostSettings.json
 }
 
@@ -45,6 +66,8 @@ setServerGameSettings() {
     WRITE_DIR=$SETTINGS_DIR
 
     echo "Using env vars for ServerGameSettings"
+
+    backwards_compatability
 
     VR_GAME_MODE="${VR_GAME_MODE:-PvP}" \
     VR_ESSENCE_DRAIN_MOD="${VR_ESSENCE_DRAIN_MOD:-1.0}" \
@@ -115,7 +138,7 @@ fi
 trap onExit INT TERM KILL
 
 cd $GAME_DIR
-Xvfb :0 -screen 0 1024x768x16 &
+Xvfb :0 -screen 0 1024x768x16 -terminate &
 setsid '/launch_server.sh' &
 
 echo $!
